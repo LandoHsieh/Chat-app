2f7cfba5feff177d2d5eebde761befb4887f83fc
Lando Hsieh <landoycx113@gmail.com>
2024-04-16 22:38:19 +0800

    fix: fix bugs

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -17,9 +17,9 @@ const Login = () => {
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
       <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-        <h1 className='text-3xl font-semibold text-center text-gray-300'>
+        <h1 className='flex flex-col text-3xl font-semibold text-center text-gray-300'>
+          <span className='text-blue-500'> Lando's Chat Center</span>
           Login
-          <span className='text-blue-500'> ChatApp</span>
         </h1>
 
         <form onSubmit={handleSubmit}>
