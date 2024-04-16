0608c1896e558f8869a6d5fa287d87fb045f8903
Lando Hsieh <landoycx113@gmail.com>
2024-04-16 22:19:49 +0800

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+import path from 'path'
+
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import {PORT, MONGO_DB_URI} from './config/index.js'
@@ -12,6 +14,7 @@ import { app, server } from './socket/socket.js'
 
 console.log(MONGO_DB_URI)
 
+const __dirname = path.resolve()
 
 app.use(express.json())
  
@@ -21,6 +24,12 @@ app.use('/api/1.0/auth', authRoutes)
 app.use('/api/1.0/messages', messageRoutes)
 app.use('/api/1.0/users', userRoutes)
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+// 以下設定為部署專用，當你進入＊（任何除了上方/api/的route），優先導到index.html畫面
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 app.get("/", (req, res) => {
     res.send("hello")
