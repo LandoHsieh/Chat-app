c843067bba6f09282ac89021dd8944218d5473ff
Lando Hsieh <landoycx113@gmail.com>
2024-04-16 22:56:49 +0800

    fix: fix socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
     const {authUser} = useAuthContext()
     useEffect(() => {
         if(authUser){
-            const socket = io('http://localhost:3001', {
+            const socket = io('https://chat-app-mbyr.onrender.com', {
                 query:{
                     userId: authUser._id
                 }
